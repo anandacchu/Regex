deb25e03fa27316846757e0269fd687a04f43dab
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-08-27 08:53:22 +0530

    uc1

---
+++ Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    public class Pattern
+    {
+        public static string REGEX_PINCODE = "[1-9] {1} [0-9] {2} [' ']?[0-9] {3}";
+
+
+        public bool validatePincode(string pincode)
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE);
+        }
+    }
+}
