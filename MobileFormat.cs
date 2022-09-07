f39e9ce77c5a47ac7ddb3f6f756610f0ab188198
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-09-07 12:10:17 +0530

    Update MobileFormat.cs

--- MobileFormat.cs
+++ MobileFormat.cs
@@ -11,10 +11,14 @@ namespace RegexDemo
     {
         public static string USER_FIRST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
         //public static string USER_LAST_NAME = "^[A-Z]{1}[A-Za-z]{2,}$";
-        public static string USER_MOBILE_FORMAT = "91 9600813037";
+        public static string USER_MOBILE_FORMAT =  "^([+][0-9]{2}[ ]){0,1}[6-9]{1}[0-9]{9}$";
         public bool FirstName(string firstName)
         {
             return Regex.IsMatch(firstName, USER_FIRST_NAME);
+        }
+          public bool MobileNumber(string mobileNumb)
+        {
+            return Regex.IsMatch(mobileNumb, MOBILE_NUMBER);
         }
     }
 }
