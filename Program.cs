4b30f294ada40204eaf1c56de29c167aa2b52c10
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-09-07 13:26:06 +0530

    UC6

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RegexDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the First Name ");
+            string firstName = Console.ReadLine();
+
+            UserRegi user = new UserRegi();
+            bool val = user.FirstName(firstName);
+
+            if (val)
+            {
+                Console.WriteLine("Pattern Matched");
+            }
+            else
+            {
+                Console.WriteLine("Pattern Not Matched");
+            }
+        }
+    }
+}
