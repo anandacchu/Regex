e5af53c4e42ed36f9c03c4e30ffb910db9144d21
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-11-05 22:56:24 +0530

    welcome

--- Program.cs
+++ Program.cs
@@ -1,23 +1,10 @@
-﻿namespace RegexDemo
+namespace UserReg
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the First Name ");
-            string firstName = Console.ReadLine();
-
-            UserRegi user = new UserRegi();
-            bool val = user.FirstName(firstName);
-
-            if (val)
-            {
-                Console.WriteLine("Pattern Matched");
-            }
-            else
-            {
-                Console.WriteLine("Pattern Not Matched");
-            }
+            Console.WriteLine("Welcome to User Registration Program");
         }
     }
 }
