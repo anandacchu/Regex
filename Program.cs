cf66595c7d885497a185a92f2398ad35846e4fbc
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-11-05 23:12:22 +0530

    uc7

--- Program.cs
+++ Program.cs
@@ -4,19 +4,17 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the Numeric Numbers ");
-            string Numeric = Console.ReadLine();
+            Console.WriteLine("Please enter password:  ");
+            string pwd = Console.ReadLine();
 
-            UserRegi user = new UserRegi();
-            bool val = user.NumericNumbers(Numeric);
-
+            bool val = user.Password(pwd);
             if (val)
             {
                 Console.WriteLine("Pattern Matched");
             }
             else
             {
-                Console.WriteLine("Pattern Not Matched");
+                Console.WriteLine("Pattern Not Matched, there must be 8 characters and at least 1 uppercase, 1 digit in character.");
             }
         }
     }
